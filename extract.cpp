63dadaca32ce347cf44f2d19459f00d8bc5544b2
dubnikov <diml@yahoo.com>
2018-08-21 14:25:58 -0300

    Update extract.cpp

--- extract.cpp
+++ extract.cpp
@@ -5,6 +5,7 @@ using namespace std;
 // Gen. pseudo random number for further auth use.
 
 int genPse(int x) {
+    srand(666);
     for (int x = 1; x <= 10; x++) {
         cout << 1 + (rand() % 6) << endl;
     }
