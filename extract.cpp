ebe4e2abe2a36afb97ebb115c5ee346e9f7db3d3
dubnikov <diml@yahoo.com>
2018-08-21 14:08:13 -0300

    Create extract.cpp

---
+++ extract.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+// Gen. pseudo random number for 
+
+int genPse(int x) {
+    for (int x = 1; x <= 10; x++) {
+        cout << 1 + (rand() % 6) << endl;
+    }
+}
+
+
+int main (int x) {
+    genPse(x);
+}
+
+// to be cont.
